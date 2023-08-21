96e930dbbd90a319cfbf798cb1d866bddb390d98
Danielle Jackson <djackson@dialexa.com>
2023-08-21 14:50:16 -0700

    Demo swagger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { startApp } from "flexure";
 import { RegisterRoutes } from './generated/routes';
+import * as swaggerJson from './generated/swagger.json';
 
-startApp(RegisterRoutes);
+startApp(RegisterRoutes, swaggerJson);
